037575cb73e48740bf40cf5c06d5f1b1cb2722fc
Robert Ridley <robert.n.ridley@gmail.com>
2018-02-23 22:38:49 +0800

    organise books according to their shelf on backend. Store this in react state

--- src/App.js
+++ src/App.js
@@ -7,20 +7,45 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    books: []
+    books: [],
+    shelves: {
+      currentlyReading: [],
+      wantToRead: [],
+      read: [],
+      none: []
+    }
+  }
+
+  updateShelves = () => {
+      this.setState({
+        shelves: {
+          currentlyReading: this.state.books.filter(book => book.shelf === 'currentlyReading'),
+          wantToRead: this.state.books.filter(book => book.shelf === 'wantToRead'),
+          read: this.state.books.filter(book => book.shelf === 'read'),
+          none: this.state.books.filter(book => book.shelf === 'none')
+        }
+    })
   }
 
   componentDidMount(){
     BooksAPI.getAll().then((books) =>
       this.setState({ books })
     )
+    .then(() =>
+      this.updateShelves()
+    )
   }
 
   render() {
-    console.log(this.state.books)
+    console.log(this.state.shelves)
     return (
       <div className="app">
-        <Route exact path="/" component={ MyBooks } />
+        <Route exact path="/" render={() => 
+          <MyBooks
+            books={this.state.books}
+            shelves={this.state.shelves}
+          /> 
+        }/>
         <Route path="/search" component={ Search } />
       </div>
     )
--- src/pages/MyBooks.js
+++ src/pages/MyBooks.js
@@ -2,30 +2,31 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Shelf from '../components/Shelf'
 
-class MyBooks extends React.Component {
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <Shelf
-            header="Currently Reading"
-          />
-          <Shelf
-            header="Want to Read"
-          />
-          <Shelf
-            header="Read"
-          />
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
+const MyBooks = (props) => {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    )
-  }
+      <div className="list-books-content">
+        <Shelf
+          header="Currently Reading"
+          bookshelf={props.shelves.currentlyReading}
+        />
+        <Shelf
+          header="Want to Read"
+          bookshelf={props.shelves.wantToRead}
+        />
+        <Shelf
+          header="Read"
+          bookshelf={props.shelves.read}
+        />
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  )
 }
 
 export default MyBooks;
